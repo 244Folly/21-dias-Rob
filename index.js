87730ef625d6779b9c7ad70cd39177190cdbe551
roberto zopelaro <robertocarlosfolly23@gmail.com>
2024-01-14 21:49:41 +0000

    main

---
+++ index.js
@@ -0,0 +1,16 @@
+let Nome = '0';
+let Idade = '0';
+let Endereço = '0';
+let CódigoPostal = '0'
+
+
+nome = prompt('O seu nome é');
+Idade = prompt('A sua idade é');
+Endereço = prompt('O seu endereço é');
+CódigoPostal = prompt ('O seu código postal é');
+
+console.log('o seu nome é : '+ CódigoPostal);
+console.log('O seu nome é : ' + nome);
+console.log('A sua idade é : ' + Idade);
+console.log('O seu endereço é : ' + Endereço);
+

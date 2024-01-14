ad4a82fcb54e30b0974bbda3dbd8baecacd86ceb
roberto zopelaro <robertocarlosfolly23@gmail.com>
2024-01-14 13:41:04 +0000

    Primeiro Commit

---
+++ dia 4/index.js
@@ -0,0 +1,7 @@
+let resposta = prompt('Me informe seu cpf:');
+let cnh = prompt('Me informe sua cnh');
+let Sus = prompt('Me informe seu crtão do Sus');
+
+console.log('O seu cartão do Sus é:', Sus);
+console.log('sua cnh é:', cnh);
+console.log('O seu cpf é:', resposta);
